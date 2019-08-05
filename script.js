df624fb6e69632a2bbc4ec0f6c20386afb30584f
manpreet singh <manpreet.singh10196@gmail.com>
2019-08-05 10:06:08 +0530

    add functionality of next player

--- script.js
+++ script.js
@@ -45,6 +45,51 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	
 });
 
+function nextPlayer(){
+	activePlayer === 0 ? activePlayer = 1: activePlayer =0;
+		roundScore=0;
+		
+		document.getElementById('current-0').textContent='0';
+		document.getElementById('current-1').textContent='0';
+		
+		/*document.querySelector('player-0-panel').classList.remove('active');
+		document.querySelector('player-1-panel').classList.add('active');*/
+		
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		
+		document.querySelector('.dice').style.display= 'none';
+	
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	if(gamePlaying){
+	//add Current score to Global score
+	scores[activePlayer] += roundScore;
+	
+	
+	// Update the Ui
+   document.querySelector('#score-'+ activePlayer).textContent=scores[activePlayer]; 
+	
+	
+	
+	//check if player win the game
+	if(scores[activePlayer] >=20){
+		document.querySelector('#name-'+activePlayer).textContent= 'Winner!';
+		document.querySelector('.dice').style.display='none';
+		document.querySelector('.player-'+activePlayer+ '-panel').classList.add('winner');
+		document.querySelector('.player-'+activePlayer+ '-panel').classList.remove('active');
+		gamePlaying=false;
+	}
+	else{
+		//nextPlayer
+	nextPlayer();
+	}	
+		
+	}
+	
+	
+});
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
